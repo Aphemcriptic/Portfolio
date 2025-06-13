3776d60f126c7de922ef47f7210b6626fda601cf
Aphemcriptic <208139979+Aphemcriptic@users.noreply.github.com>
2025-06-13 11:53:29 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+function burgerOpen() {
+    let menu = document.getElementById("nav-list");
+    if (menu.style.display === "block") {
+      menu.style.display = "none";
+    } else {
+      menu.style.display = "block";
+    }
+}
+
+function closeMenu(){
+let menu = document.getElementById("nav-list");
+  if (menu.style.display === "block") {
+      menu.style.display = "none";}
+}
+
+  
